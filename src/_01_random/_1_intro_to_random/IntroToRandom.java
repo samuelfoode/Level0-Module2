0ba7ced87dcd2cad2ea8c9fa757aa38f5093517b
lvl0_module2 <Eddiejhunter1000000@gmail.com>
2025-10-03 16:57:39 -0700

    ';;';'

--- src/_01_random/_1_intro_to_random/IntroToRandom.java
+++ src/_01_random/_1_intro_to_random/IntroToRandom.java
@@ -30,7 +30,7 @@ public class IntroToRandom {
 		for (int i=0; i<5; i++) {
 			// Uncomment the line of code below, but replace the _____ with a positive integer (e.g. 100) before running the program			
 
-			// number = ran.nextInt(_____);
+			number = ran.nextInt(100);
 			System.out.println(number);
 			
 		}
@@ -49,7 +49,7 @@ public class IntroToRandom {
 			// For our example, this will be:  ( 75 - 25 + 1 )
 			// Also replace <lowest value> with the start of the range.  For our example, this is 25.
 			
-			// number = ran.nextInt(_____) + <lowest value>;
+			number = ran.nextInt(8 - -222 + 1) + -222;
 			System.out.println(number);
 			
 		}
